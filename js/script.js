f02a862af5850077fdcde2b0c8c2b41122a9a0be
25Simone <dantsimone111@gmail.com>
2022-01-12 13:34:56 +0100

    milestone 1 e 2 completate e impostazione milestone 3

---
+++ js/script.js
@@ -0,0 +1,141 @@
+const iconsList = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+// definisco la variabile di collegamento all'html
+const outputHtml = document.getElementById('output');
+
+const selectType = document.getElementById('select-type');
+
+// definisco la funzione per stampare le icone
+function printIcon(object){
+    const upperName = object.name.toUpperCase();
+    outputHtml.innerHTML += `
+    <div class="box">
+        <i class="${object.color} ${object.family} ${object.prefix}${object.name}"></i>
+        <span class="icon-name">${upperName}</span>
+    </div>`;
+}
+
+// imposto il forEach per stampare le icone
+iconsList.forEach((icon) => {
+    printIcon(icon);
+})
+
+selectType.addEventListener('change', () => {
+	const animalIconsList = iconsList.filter((icon) => {
+		return icon.type === 'animal';
+	})
+	console.log(animalIconsList); //debug
+})
